6da047b27f904bc7fb34113d1b8db8ce374da7b8
Goizane <goizaneolaneta@gmail.com>
2023-06-23 17:40:53 +0200

    LoadingWidget done

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:note_app/core/controllers/theme_controller.dart';
 import 'package:note_app/core/models/notes.dart';
 import 'package:note_app/ui/widgets/cards/custom_cards.dart';
+import 'package:note_app/ui/widgets/loading_widget/loading_widget_controller.dart';
+import 'package:note_app/ui/widgets/loading_widget/loading_widget.dart';
 
 GlobalKey<ScaffoldState> homePageKey = GlobalKey<ScaffoldState>();
 GlobalKey<ScaffoldMessengerState> homePageMessengerKey = GlobalKey<ScaffoldMessengerState>();
@@ -18,46 +20,65 @@ class _HomePageState extends State<HomePage> {
   @override
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: ThemeController.instance.brightness,
-      builder: (BuildContext context, value, Widget? child) {
-        final theme = ThemeController.instance;
-        return ScaffoldMessenger(
-          key: homePageMessengerKey,
-          child: Scaffold(
-            backgroundColor: theme.background(),
-            key: homePageKey,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: Text(
-                    "Home Page",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, color: theme.primary()),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () => theme.changeTheme(),
-                  child: const Text('Action'),
-                ),
-                Row(
+    return Stack(
+      children: [
+        ValueListenableBuilder(
+          valueListenable: ThemeController.instance.brightness,
+          builder: (BuildContext context, value, Widget? child) {
+            final theme = ThemeController.instance;
+            return ScaffoldMessenger(
+              key: homePageMessengerKey,
+              child: Scaffold(
+                backgroundColor: theme.background(),
+                key: homePageKey,
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Flexible(child: SimpleCard(note)),
-                    Flexible(child: ImageCard(note1)),
+                    Center(
+                      child: Text(
+                        "Home Page",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: theme.primary()),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => theme.changeTheme(),
+                      child: const Text('Action'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        LoadingWidgetController.instance.loading();
+                        LoadingWidgetController.instance.changeText("Esta cargando...");
+                        await Future.delayed(const Duration(seconds: 2));
+                        LoadingWidgetController.instance.close();
+                      },
+                      child: const Text('Loading'),
+                    ),
+                    Row(
+                      children: [
+                        Flexible(child: SimpleCard(note)),
+                        Flexible(child: ImageCard(note1)),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Flexible(child: SimpleCard(note)),
+                        Flexible(child: TextImageCard(note2)),
+                      ],
+                    ),
                   ],
                 ),
-                Row(
-                  children: [
-                    Flexible(child: SimpleCard(note)),
-                    Flexible(child: TextImageCard(note2)),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+              ),
+            );
+          },
+        ),
+        ValueListenableBuilder(
+          valueListenable: LoadingWidgetController.loadingNotifier,
+          builder: (context, bool value, Widget? child) {
+            return value ? const LoadingWidget() : const SizedBox();
+          },
+        ),
+      ],
     );
   }
 }
---
+++ lib/ui/widgets/loading_widget/loading_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/core/controllers/theme_controller.dart';
+import 'package:note_app/ui/widgets/loading_widget/loading_widget_controller.dart';
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final contentValue = LoadingWidgetController.instance.contentValue();
+
+    return Material(
+      color: Colors.white24,
+      child: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              color: ThemeController.instance.accent(),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Cargando',
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.white),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  contentValue,
+                  style: const TextStyle(fontSize: 16, color: Colors.white54),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
