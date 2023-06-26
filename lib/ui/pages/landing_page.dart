5d96220212eae66a17a78f51431b312e383c54ec
Goizane <goizaneolaneta@gmail.com>
2023-06-26 20:50:03 +0200

    landingPage done

--- lib/ui/pages/landing_page.dart
+++ lib/ui/pages/landing_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:note_app/core/constants/data.dart';
 import 'package:note_app/ui/pages/home_page.dart';
 import 'package:note_app/ui/widgets/buttons/simple_buttons.dart';
+import 'package:note_app/ui/widgets/loading_widget/loading_widget.dart';
+import 'package:note_app/ui/widgets/loading_widget/loading_widget_controller.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
@@ -18,56 +20,66 @@ class LandingPage extends StatelessWidget {
     );
   }
 
-  Future<void> initMethods(context) async {
-    Navigator.pushNamed(context, HomePage.homePageRoute);
-  }
+  Future<void> initMethods(context) async {}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Flexible(
-              child: _image(),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  Text(
-                    Constants.mainTitle,
-                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          fontWeight: FontWeight.bold,
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: Colors.white,
+          body: SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: _image(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      Text(
+                        Constants.mainTitle,
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        Constants.subtitle,
+                        style: TextStyle(color: Colors.blueGrey),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 100),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: MediumButton(
+                          title: "Ingresar",
+                          onTap: () async {
+                            LoadingWidgetController.instance.loading();
+                            await initMethods(context);
+                            LoadingWidgetController.instance.close();
+                            Navigator.pushNamed(context, HomePage.homePageRoute);
+                          },
                         ),
+                      ),
+                      const SizedBox(height: 60),
+                    ],
                   ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    Constants.subtitle,
-                    style: TextStyle(color: Colors.blueGrey),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 100),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 16),
-                    child: MediumButton(
-                      title: "Ingresar",
-                      onTap: () async {
-                        await initMethods(context);
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 60),
-                ],
-              ),
-            )
-          ],
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+        ValueListenableBuilder(
+            valueListenable: LoadingWidgetController.loadingNotifier,
+            builder: (context, bool value, Widget? child) {
+              return value ? const LoadingWidget() : const SizedBox();
+            })
+      ],
     );
   }
 }
